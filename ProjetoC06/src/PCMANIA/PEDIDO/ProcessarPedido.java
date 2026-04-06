ad2bb592a3924c60a636d04a6e0917ab02df6683
Felipe Lamy <82849597+felpsman@users.noreply.github.com>
2026-04-06 00:03:42 -0300

    ProcessarPedido.java

--- ProjetoC06/src/PCMANIA/PEDIDO/ProcessarPedido.java
+++ ProjetoC06/src/PCMANIA/PEDIDO/ProcessarPedido.java
@@ -1,5 +1,6 @@
 package PCMANIA.PEDIDO;
 import PCMANIA.Cliente.Cliente;
+//Classe criada para mostrar o processamento do pedido (Valor total, quais pcs foram comprados)
 public class ProcessarPedido {
     public void processar(Cliente cliente) {
         System.out.println("\n=== RESUMO DO PEDIDO ===");
